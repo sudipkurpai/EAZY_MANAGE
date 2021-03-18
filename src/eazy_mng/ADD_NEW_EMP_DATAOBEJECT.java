486a837052ebec8216a4fe827f4ae3c5e3872d31
Sudip Maiti <Sudip Maiti@Sudip_Maiti>
2021-03-18 21:26:31 +0530

    EMNGS18032021

---
+++ src/eazy_mng/ADD_NEW_EMP_DATAOBEJECT.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eazy_mng;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.*;
+
+/**
+ *
+ * @author Sudip Maiti
+ */
+public class ADD_NEW_EMP_DATAOBEJECT {
+    public static int inventory_management (String FIRST_NAME, String LAST_NAME, String EMAIL, 
+            String MOBILE_NO, String EMP_ID, String PASSWORD, String CONFIRM_PASSWORD, 
+            String DOB,String ADDRESS, String GENDER,String EMP_ADD_MNG_NAME,String EMP_ADD_MNG_ID, String ADD_TIME,String ADD_DATE){
+        int status=0;
+              try{
+                   Connection con=DATABASE_CONNECTION.getConnection();  
+                   PreparedStatement ps=con.prepareStatement("INSERT INTO employee_register (FIRST_NAME,LAST_NAME,EMAIL,MOBILE_NO,"
+                           + "EMP_ID,PASSWORD,CONFIRM_PASSWORD,DOB,ADDRESS,GENDER,EMP_ADD_MNG_NAME,EMP_ADD_MNG_ID,ADD_TIME,ADD_DATE) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?)");
+                   ps.setString(1, FIRST_NAME);
+                   ps.setString(2, LAST_NAME);
+                   ps.setString(3, EMAIL);
+                   ps.setString(4, MOBILE_NO);
+                   ps.setString(5, EMP_ID);
+                   ps.setString(6, PASSWORD);
+                   ps.setString(7, CONFIRM_PASSWORD);
+                   ps.setString(8, DOB);
+                   ps.setString(9, ADDRESS);
+                   ps.setString(10, GENDER);
+                   ps.setString(11, EMP_ADD_MNG_NAME);
+                   ps.setString(12, EMP_ADD_MNG_ID);
+                   ps.setString(13, ADD_TIME);
+                   ps.setString(14, ADD_DATE);                   
+                   status=ps.executeUpdate();
+                   con.close();
+              }catch(Exception e){
+                  System.out.println(e);
+              }
+              System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!"+status);
+              return status;
+
+//INSERT INTO `register`(`FIRST_NAME`, `LAST_NAME`, `EMAIL`, `MOBILE_NO`, `PASSWORD`, `CONFIRM_PASSWORD`, `ADDRESS`, `GENDER`) 
+    }
+    public static boolean validate(String email,String password) throws SQLException
+    {
+        boolean status =false;
+        try{
+            Connection con=DATABASE_CONNECTION.getConnection();
+            PreparedStatement ps=con.prepareStatement("select * from employee_register where Emp_Id = ? and Password=?");
+            ps.setString(1, email);
+            ps.setString(2, password);
+            ResultSet rs=ps.executeQuery();
+            status=rs.next();
+            con.close();
+           }
+        catch(Exception e){System.out.println(e);}
+        return status;
+    }
+}
---
+++ src/eazy_mng/UPDATEPROFILE_M_DATAOBJECT.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eazy_mng;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author RAGHUNATH DAS
+ */
+public class UPDATEPROFILE_M_DATAOBJECT {
+    
+      public static int updateprofile_m ( String DOB, String ADDRESS,String GENDER,String mng_id){
+        int status=0;
+              try{
+                   Connection con=DATABASE_CONNECTION.getConnection();  
+                   PreparedStatement ps=con.prepareStatement("UPDATE register set DOB =?,ADDRESS =?,GENDER =? where MNG_ID = ?");
+                   
+                   ps.setString(1, DOB);
+                   ps.setString(2, ADDRESS);
+                   ps.setString(3, GENDER);
+                   
+                   ps.setString(4, mng_id);
+                   status=ps.executeUpdate();
+                   con.close();
+              }catch(Exception e){
+                  System.out.println(e);
+              }
+              System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!"+status);
+              return status;
+
+//INSERT INTO `register`(`FIRST_NAME`, `LAST_NAME`, `EMAIL`, `MOBILE_NO`, `PASSWORD`, `CONFIRM_PASSWORD`, `ADDRESS`, `GENDER`) 
+    }
+   
+    public static int update_password (String Pass,String c_pass,String mng_id){
+        int status=0;
+              try{
+                   Connection con=DATABASE_CONNECTION.getConnection();  
+                   PreparedStatement ps=con.prepareStatement("UPDATE register set PASSWORD =?,CONFIRM_PASSWORD =? where MNG_ID = ?");
+                   
+                   
+                   ps.setString(1, Pass);
+                   ps.setString(2, c_pass);
+                   ps.setString(3, mng_id);
+                   
+                   status=ps.executeUpdate();
+                   con.close();
+              }catch(Exception e){
+                  System.out.println(e);
+              }
+              System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!"+status);
+              return status;
+              
+
+    }
+               public static int Reset_password (String Pass,String c_pass,String email){
+        int status=0;
+              try{
+                   Connection con=DATABASE_CONNECTION.getConnection();  
+                   PreparedStatement ps=con.prepareStatement("UPDATE register set PASSWORD =?,CONFIRM_PASSWORD =? where EMAIL = ?");
+                   
+                   
+                   ps.setString(1, Pass);
+                   ps.setString(2, c_pass);
+                   ps.setString(3, email);
+                  // ps.setString(4, phone);
+                   
+                   status=ps.executeUpdate();
+                   con.close();
+              }catch(Exception e){
+                  System.out.println(e);
+              }
+              System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!"+status);
+              return status;
+//INSERT INTO `register`(`FIRST_NAME`, `LAST_NAME`, `EMAIL`, `MOBILE_NO`, `PASSWORD`, `CONFIRM_PASSWORD`, `ADDRESS`, `GENDER`) 
+    }
+}
+
+
+
